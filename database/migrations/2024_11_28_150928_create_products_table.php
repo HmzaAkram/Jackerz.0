4ab32cd9da672ffa3e3ce966dcfe03f80f91fd88
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-13 15:39:50 +0500

    working

--- database/migrations/2024_11_28_150928_create_products_table.php
+++ database/migrations/2024_11_28_150928_create_products_table.php
@@ -12,27 +12,23 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
-        if (!Schema::hasTable('products')) {
-            Schema::create('products', function (Blueprint $table) {
-                $table->id();
-                $table->string('title');
-                $table->string('image');
-                $table->string('image1');
-                $table->string('image2');
-                $table->string('image3');
-                $table->string('category');
-                $table->string('quantity');
-                $table->string('price');
-                $table->string('discount_price');
-                $table->string('material');
-                $table->string('size');
-                $table->string('color');
-                $table->string('design_type');
-                $table->timestamps();
-            });
-        }
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('category');
+        $table->string('material');
+        $table->string('size');
+        $table->string('color');
+        $table->string('design');
+        $table->text('description')->nullable();
+        $table->decimal('price', 8, 2);
+        $table->decimal('discount_price', 8, 2)->nullable();
+        $table->integer('quantity');
+        $table->timestamps();
+    });
+}
+
     
 
     /**
