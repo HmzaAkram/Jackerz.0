29da736cfe9839dc02780eceb978c86028ebeab4
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-30 23:15:03 +0500

    Update about.blade.php

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -11,13 +11,9 @@
     <meta property="og:type" content="">
     <meta property="og:url" content="">
     <meta property="og:image" content="">
-    <link rel="shortcut icon" type="image/x-icon" href="user/assets/imgs/theme/favicon.ico">
-    <link rel="stylesheet" href="user/assets/css/main.css">
-    <link rel="stylesheet" href="user/assets/css/custom.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
-<!-- @include('user.header') -->
+
     <main class="main">
         <H1>About</H1>
     </main>
